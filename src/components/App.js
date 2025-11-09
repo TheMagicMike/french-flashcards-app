23ab6ba523cdebb9c4533aab35bda75410841ce3
TheMagicMike <michaeldillon@hotmail.co.uk>
2025-11-09 17:06:45 +0000

    Fix App.js - Remove unused useEffect import

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 // Embedded vocabulary data - sample words for each theme/difficulty
 const VOCAB_DATA = {
